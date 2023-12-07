573b7129aa611a0e15da25048142ec9de015f88f
Abhishek Pandey <abhishekpandey118852@gmail.com>
2023-12-07 13:11:59 +0530

    commit

---
+++ Javascript_Tutorial/first.js
@@ -0,0 +1,7 @@
+let user = "Abhishek";
+let age = 18;
+console.log(`The user's name is ${user} and his age is ${age}`);
+if(age>=18)
+console.log("The person is married");
+else
+console.log("The person is not married");
---
+++ Javascript_Tutorial/loops.js
@@ -0,0 +1,4 @@
+let ans = 10;
+for(let i of ans){
+    console.log(i)
+}
